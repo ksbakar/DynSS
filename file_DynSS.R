c881ec3cde41ca0419360c2a695f14fe1b0e7804
Shuvo Bakar <shuvo.bakar@gmail.com>
2022-12-05 10:46:45 +1100

    Add files via upload

---
+++ file_DynSS.R
@@ -0,0 +1,29 @@
+
+################################################################################
+################################################################################
+
+## implementation file for DynSS method
+## to reproduce the results in the paper
+
+source("source_file_DynSS.R")
+
+
+fig_1()
+## Figure 2
+fig_2()
+## Figure 3
+fig_3()
+## Figure 4
+fig_4()
+## Figure 5
+fig_5()
+## Figure 6
+fig_6()
+## Figure 7
+fig_7(margin_of_error = 0.1) # 
+fig_7(margin_of_error = 0.05) # not reported in the paper
+##
+
+
+################################################################################
+################################################################################
