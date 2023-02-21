5f9a109ca5d4287aba5387987adf3dc7831bf9aa
Shuvo Bakar <shuvo.bakar@gmail.com>
2023-02-21 15:40:45 +1100

    Add files via upload

--- file_DynSS.R
+++ file_DynSS.R
@@ -5,9 +5,31 @@
 ## implementation file for DynSS method
 ## to reproduce the results in the paper
 
+library(devtools)
+library(roxygen2)
+## Needed because this file has roxygen2 comments. Otherwise you get a
+## 'could not find function 'digest'' error
+source_url("https://github.com/ksbakar/DynSS/blob/main/source_file_DynSS.R?raw=TRUE")
 source("source_file_DynSS.R")
 
+package_list <- c("dplyr", "data.table", "spTimer", "LaplacesDemon", "extraDistr",
+                  "pbapply", "MASS", "ggplot2", "grid", "gridExtra", "ggridges")
+checkPkg <- function(package_list){
+  check_installed <- package_list[!(package_list %in% installed.packages()[ , "Package"])]
+  if(length(check_installed)) {
+    print("Installing required packages from CRAN ...")
+    install.packages(check_installed)
+    print("All required packages are installed in the working machine ...")
+  }
+  else{
+    print("All required packages are installed in the working machine ...")
+  }
+}
+checkPkg(package_list)
+lapply(package_list, require, character.only = TRUE)
 
+
+## Figure 1
 fig_1()
 ## Figure 2
 fig_2()
@@ -17,11 +39,6 @@ fig_3()
 fig_4()
 ## Figure 5
 fig_5()
-## Figure 6
-fig_6()
-## Figure 7
-fig_7(margin_of_error = 0.1) # 
-fig_7(margin_of_error = 0.05) # not reported in the paper
 ##
 
 
@@ -30,48 +47,30 @@ fig_7(margin_of_error = 0.05) # not reported in the paper
 
 ## Code to reproduce results in the Supplementary section
 
-## Figure S2
-fig_S2(D=list(c(0.05,0.95)))
-fig_S2(D=list(c(0.01,0.99)))
-fig_S2(D=list(c(0.1,0.9))) # not included in the paper
-
-## Figure S3
-fig_S3(D=list(c(0.05,0.95)),margin_of_error=0.05)
-fig_S3(D=list(c(0.1,0.9)))
-fig_S3(D=list(c(0.01,0.99)))
-
-## Figure S4
-fig_S4 <- function(D0=0.025,D1=0.975){
-  fig_6(drop_arm_type=c("futility"),D0=D0,D1=D1)
-}
-fig_S4(D0=0.1,D1=0.9)
-fig_S4(D0=0.05,D1=0.95)
-fig_S4(D0=0.01,D1=0.99)
-
-## Figure S5
-fig_S5 <- function(D0=0.025,D1=0.975,margin_of_error=0.10){
-  fig_7(drop_arm_type=c("futility"),D0=D0,D1=D1,margin_of_error = margin_of_error)
-}
-fig_S5(D0=0.1,D1=0.9)
-fig_S5(D0=0.05,D1=0.95)
-fig_S5(D0=0.01,D1=0.99)
-
-## Figure S6
-fig_S6 <- function(D0=0.025,D1=0.975){
-  fig_6(drop_arm_type=c("efficacy"),D0=D0,D1=D1)
-}
-fig_S6(D0=0.1,D1=0.9)
-fig_S6(D0=0.05,D1=0.95)
-fig_S6(D0=0.01,D1=0.99)
-
-## Figure S7
-fig_S7 <- function(D0=0.025,D1=0.975,margin_of_error=0.10){
-  fig_7(drop_arm_type=c("efficacy"),D0=D0,D1=D1,margin_of_error = margin_of_error)
-}
-fig_S7(D0=0.1,D1=0.9)
-fig_S7(D0=0.05,D1=0.95)
-fig_S7(D0=0.01,D1=0.99)
-
+## Fig S1
+fig_S1()
+## Fig S3
+fig_S3(D=list(c(0.05,0.95)))
+fig_S3(D=list(c(0.05,0.99)))
+## Fig S4
+fig_S4(margin_of_error=0.05)
+fig_S4(margin_of_error=0.01)
+## Fig S5
+fig_4(drop_arm_type=c("futility"),D0=0.1,D1=0.9)
+fig_4(drop_arm_type=c("futility"),D0=0.05,D1=0.95)
+fig_4(drop_arm_type=c("futility"),D0=0.01,D1=0.99)
+## Fig S6
+fig_5(drop_arm_type=c("futility"),D0=0.1,D1=0.9)
+fig_5(drop_arm_type=c("futility"),D0=0.05,D1=0.95)
+fig_5(drop_arm_type=c("futility"),D0=0.01,D1=0.99)
+## Fig S7
+fig_4(drop_arm_type=c("efficacy"),D0=0.1,D1=0.9)
+fig_4(drop_arm_type=c("efficacy"),D0=0.05,D1=0.95)
+fig_4(drop_arm_type=c("efficacy"),D0=0.01,D1=0.99)
+## Fig S8
+fig_5(drop_arm_type=c("efficacy"),D0=0.1,D1=0.9)
+fig_5(drop_arm_type=c("efficacy"),D0=0.05,D1=0.95)
+fig_5(drop_arm_type=c("efficacy"),D0=0.01,D1=0.99)
 
 ################################################################################
 ################################################################################
